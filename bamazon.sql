53c1a6e2531f20f968d5bfb63f844e2c055517e5
Des <desinoelle@gmail.com>
2018-11-27 01:57:30 -0500

    Listed products for purchase from database.

--- bamazon.sql
+++ bamazon.sql
@@ -15,15 +15,13 @@ CREATE TABLE products (
 
 -- ALL AVAILABLE PRODUCTS
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (0111, "Stunner Thigh-High Boots", "Shoes", 59.99, 5),
-(0123, "Vans High-Top Sneakers", "Shoes", 39.99, 6),
-(0257, "BombShell BodyCon Dress", "Apparel", 29.99, 8),
-(0279, "CuddleMe Faux Fur Jacket", "Apparel", 49.99, 5),
-(0456, "Boogie Nights Record Player", "Electronics", 99.99, 10),
-(0412, "Samsung 55' HDTV", "Electronics", 499.99, 4),
-(0835, "Boho Gal Dream Catcher", "Home Decor", 14.99, 15),
-(0832, "Boho Gal Queen-Size Comforter Set", "Home Decor", 129.99, 9),
-(0987, "Modern Side Table", "Furniture", 99.99, 4),
-(0988, "Modern Dresser", "Furniture", 199.99, 4);
-
-SELECT * FROM products;
+VALUES (111, "Stunner Thigh-High Boots", "Shoes", 59.99, 5),
+(123, "Vans High-Top Sneakers", "Shoes", 39.99, 6),
+(257, "BombShell BodyCon Dress", "Apparel", 29.99, 8),
+(279, "CuddleMe Faux Fur Jacket", "Apparel", 49.99, 5),
+(456, "Boogie Nights Record Player", "Electronics", 99.99, 10),
+(412, "Samsung 55' HDTV", "Electronics", 499.99, 4),
+(835, "Boho Gal Dream Catcher", "Home Decor", 14.99, 15),
+(832, "Boho Gal Queen-Size Comforter Set", "Home Decor", 129.99, 9),
+(987, "Modern Side Table", "Furniture", 99.99, 4),
+(988, "Modern Dresser", "Furniture", 199.99, 4);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,4 +7,37 @@ var mysql = require("mysql");
 // mySQL Password
 var mySQLPass = keys.mySQLPass;
 
-console.log(mySQLPass);
+// mySQL connection (add your own user info and password)
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: mySQLPass,
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+
+    console.log("Connected as ID " + connection.threadId);
+    listProducts();
+    buyProducts();
+    connection.end();
+});
+
+// This function lists all available products
+function listProducts() {
+    console.log("Items available for purchase:");
+    connection.query("SELECT * FROM products", function(err, data) {
+        console.log("-----------------------------------");
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].product_name + " - $" + data[i].price + " - ID: " + data[i].item_id);
+        }
+        console.log("-----------------------------------");
+    });
+}
+
+// This function allows the user to purchase products
+function buyProducts() {
+
+}
